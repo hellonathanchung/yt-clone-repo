030352b5fdd23cc27ff74ff4370eb445b6c9ed16
Nathan Chung <chung.nathanh@gmail.com>
2023-10-04 16:33:40 -0700

    adds get collection id

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -11,6 +11,17 @@ const firestore = new Firestore();
 const rawVideoBucketName = "hnc-raw-videos";
 const storage = new Storage();
 
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
+
 
 export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
   // Check if the user is authentication
@@ -37,6 +48,7 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
 
   return {url, fileName};
 });
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
@@ -47,4 +59,10 @@ export const createUser = functions.auth.user().onCreate((user) => {
   firestore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const querySnapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return querySnapshot.docs.map((doc) => doc.data());
 });
